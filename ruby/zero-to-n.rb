89c6e03d4947762c6346569acc6db1e7ffea2945
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-08-21 18:21:39 -0500

    Create zero-to-n.rb
    Print all numenbers from 0 to n following the pattern "1, -2, 3, -4, 5, -6"
    (Ruby version)

---
+++ ruby/zero-to-n.rb
@@ -0,0 +1,18 @@
+def main
+  print "Number n: "
+  n = gets.strip.to_f
+  current_number = 0
+  negative = true
+  while current_number <= n
+    if negative
+      puts "-#{current_number}"
+      negative = false
+    else
+      puts current_number
+      negative = true
+    end
+    current_number += 1
+  end
+end
+
+main
