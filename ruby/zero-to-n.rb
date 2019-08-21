0fa819b52183f3a59400518b2c19278061f846a7
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-08-21 18:26:05 -0500

    Update zero-to-n.rb
    Now it prints sequence properly

--- ruby/zero-to-n.rb
+++ ruby/zero-to-n.rb
@@ -1,11 +1,11 @@
 def main
-  print "Number n: "
+  print "Number n: ".strip
   n = gets.strip.to_f
   current_number = 0
   negative = true
   while current_number <= n
     if negative
-      puts "-#{current_number}"
+      puts -current_number
       negative = false
     else
       puts current_number
